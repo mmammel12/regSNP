1a54518fc65d7f8d3cf864c1a4bbf9b4ab2a09e9
mmammel12 <mmammel12@gmail.com>
2019-07-17 08:39:31 -0400

    fixed removal of files and added error logging

--- regsnp_intron/feature_calculator.py
+++ regsnp_intron/feature_calculator.py
@@ -15,14 +15,7 @@ from bson.json_util import dumps
 import csv
 
 from utils.vcf import VCF
-from utils.seq import FlankingSeq
 from utils.snp import SNP
-from utils.annovar import Annovar
-from utils.closest_exon import ClosestExon
-from rbp.rbp_change import RBPChange
-from protein_feature.protein_feature import ProteinFeature
-from junc_score.junction_strength import JunctionStrength
-from conservation.phylop import Phylop
 
 
 class FeatureCalculator(object):
@@ -35,9 +28,6 @@ class FeatureCalculator(object):
         self.logger = logging.getLogger(__name__)
 
     def calculate_feature(self):
-        # return boolean
-        needPredictor = False
-
         out_dir_tmp = os.path.join(self.out_dir, "tmp")
         os.mkdir(out_dir_tmp)
 
@@ -69,8 +59,8 @@ class FeatureCalculator(object):
         self.logger.info("Checking input file format.")
         snp = SNP(self.ifname)
         ref_name = os.path.join(self.db_dir, "hg19/hg19.fa")
-        if not snp.is_valid(ref_name):
-            raise RuntimeError("Invalid input file format.")
+        # if not snp.is_valid(ref_name):
+        #     raise RuntimeError("Invalid input file format.")
 
         # Sort input
         self.logger.info("Sorting input file.")
@@ -85,123 +75,11 @@ class FeatureCalculator(object):
         )
 
         # pull data from db
-        needCompute = self._queryDB()
-
-        # if self._queryDB returned true, continue with computation
-        if needCompute:
-            # Annotate SNVs
-            self.logger.info("Annotating SNVs with ANNOVAR.")
-            annovar_path = os.path.expanduser(self.settings["annovar_path"])
-            annovar_db_path = os.path.expanduser(self.settings["annovar_db_path"])
-            annovar = Annovar(annovar_path, annovar_db_path)
-            annovar.annotate(
-                os.path.join(out_dir_tmp, "snp.switched"),
-                os.path.join(out_dir_tmp, "snp"),
-            )
-
-            # Calculate distance to closest protein coding exons and extract intronic SNVs
-            self.logger.info("Calculating distance to closest protein coding exons.")
-            protein_coding_exon_fname = os.path.join(
-                self.db_dir, "hg19_ensGene_exon.bed"
-            )
-            closest_exon = ClosestExon(protein_coding_exon_fname)
-            closest_exon.get_closest_exon(
-                os.path.join(out_dir_tmp, "snp.intronic"),
-                os.path.join(out_dir_tmp, "snp.distance"),
-            )
-
-            if os.path.getsize(os.path.join(out_dir_tmp, "snp.distance")) <= 0:
-                self.logger.error("No input SNVs are in valid intronic regions.")
-                raise RuntimeError("No input SNVs are in valid intronic regions.")
-
-            # Extract flanking sequence
-            self.logger.info("Fetching flanking sequence.")
-            seq = FlankingSeq(ref_name, 20)
-            seq.fetch_flanking_seq(
-                os.path.join(out_dir_tmp, "snp.distance"),
-                os.path.join(out_dir_tmp, "snp.seq"),
-            )
-            seq.fetch_flanking_seq(
-                os.path.join(out_dir_tmp, "snp.distance"),
-                os.path.join(out_dir_tmp, "snp.fa"),
-                otype="fasta",
-            )
-            seq.close()
-
-            # Calculate RBP binding change
-            self.logger.info("Calculating RBP binding change.")
-            pssm_path = os.path.join(self.db_dir, "motif/pwm")
-            pssm_list_fname = os.path.join(self.db_dir, "motif/pwm_valid.txt")
-            ms_fname = os.path.join(self.db_dir, "motif/binding_score_mean_sd.txt")
-            rbp = RBPChange(pssm_path, pssm_list_fname, ms_fname)
-            rbp.rbps.cal_matching_score(
-                os.path.join(out_dir_tmp, "snp.seq"),
-                os.path.join(out_dir_tmp, "snp.rbp_score"),
-            )
-            rbp.cal_change(
-                os.path.join(out_dir_tmp, "snp.rbp_score"),
-                os.path.join(out_dir_tmp, "snp.rbp_change"),
-            )
-
-            # Calculate protein structural features (disorder, secondary structure, ASA, Pfam, PTM)
-            self.logger.info("Extracting protein structural features.")
-            db_fname = os.path.join(self.db_dir, "ensembl.db")
-            gene_pred_fname = os.path.join(self.db_dir, "hg19_ensGene.txt")
-            protein_feature = ProteinFeature(db_fname, gene_pred_fname)
-            protein_feature.calculate_protein_feature(
-                os.path.join(out_dir_tmp, "snp.distance"),
-                os.path.join(out_dir_tmp, "snp.protein_feature"),
-            )
-
-            # Calculate junction score
-            self.logger.info("Calculating junction strength change.")
-            donor_ic_fname = os.path.join(self.db_dir, "motif/donorsite.pssm")
-            acceptor_ic_fname = os.path.join(self.db_dir, "motif/acceptorsite.pssm")
-            junction_strength = JunctionStrength(donor_ic_fname, acceptor_ic_fname)
-            junction_strength.cal_junction_strength(
-                ref_name,
-                os.path.join(out_dir_tmp, "snp.distance"),
-                os.path.join(out_dir_tmp, "snp.junc"),
-            )
-
-            # Calculate phylop conservation score
-            self.logger.info("Calculating conservation score.")
-            phylop_fname = os.path.join(
-                self.db_dir, "phylop/hg19.100way.phyloP100way.bw"
-            )
-            phylop = Phylop(phylop_fname)
-            phylop.calculate(
-                os.path.join(out_dir_tmp, "snp.distance"),
-                os.path.join(out_dir_tmp, "snp.phylop"),
-            )
-            phylop.close()
-
-            self._merge_features()
-        # snp.switched doesn't exist
-        else:
-            # TODO change naming in _queryDB and uncomment this
-            # rename prediction.txt to snp.prediction.txt
-            # os.rename(
-            #     os.path.join(self.out_dir, "prediction.txt"),
-            #     os.path.join(out_dir_tmp, "snp.prediction.txt"),
-            # )
-            # rename prediction.json to snp.prediciton.json
-            # os.rename(
-            #     os.path.join(self.out_dir, "prediction.json"),
-            #     os.path.join(out_dir_tmp, "snp.prediction.json"),
-            # )
-            # predictor not necessary, return false
-            needPredictor = False
-        return needPredictor
+        self._queryDB()
 
     def _queryDB(self):
-        # return boolean
-        # TODO change this to optionally allow computing
-        needCalculate = False
         # tmp directory path
         out_dir_tmp = os.path.join(self.out_dir, "tmp")
-        # create tempSwitched to rewrite snp.switched
-        tempSwitched = ""
         # create headers string
         headers = ""
         # create temp json dictionary
@@ -211,7 +89,7 @@ class FeatureCalculator(object):
             "mongodb+srv://cluster0-souoy.gcp.mongodb.net/test",
             username="serverAdmin",
             password="s3cr3tpass",
-        )
+        )  # CHANGE THIS TO MONGO URI AND LOGIN CREDENTIALS
         # get the DB
         db = client.muriDB
         # get the collection
@@ -241,12 +119,11 @@ class FeatureCalculator(object):
                 queries.insert(query)
                 # if data not in db
                 if item == None:
-                    # write line to tempSwitched, tab delimited
-                    for element in cols:
-                        tempSwitched += element + "\t"
-                    # remove last \t from tempSwitched and replace with \n
-                    tempSwitched = tempSwitched[:-2] + "\n"
-                    # tempSwitched will be used to rewrite snp.switched
+                    self.logger.info(
+                        "<invalid>Error: {0} {1} {2} {3} is not a valid combination</invalid>".format(
+                            cols[0], cols[1], cols[2], cols[3]
+                        )
+                    )
                 # else, append all data to output file string, tab delimited
                 else:
                     # delete _id, it is not needed
@@ -285,14 +162,6 @@ class FeatureCalculator(object):
         json_str = json_str[:-1] + "]}"
         # remove unicode artifacts
         json_str.replace("u'", "'")
-        # if tempSwitched is empty
-        if len(tempSwitched) == 0:
-            # return true
-            needCalculate = False
-        # else, overwrite snp.switched with tempSwitched
-        else:
-            with open(inFile, "w") as switched:
-                switched.write(tempSwitched)
         # create file called snp.prediction.txt and snp.prediction.json in out_dir
         outFile = os.path.join(self.out_dir, "snp.prediction.txt")
         outJSONFile = os.path.join(self.out_dir, "snp.prediction.json")
@@ -378,79 +247,6 @@ class FeatureCalculator(object):
                             out_f.write("+\t")
                 out_f.write("\n")
             out_json_f.write(json_str)
-        return needCalculate
-
-    def _merge_features(self):
-        self.logger.info("Merging all the features.")
-        out_dir_tmp = os.path.join(self.out_dir, "tmp")
-        rbp_change = pd.read_csv(
-            os.path.join(out_dir_tmp, "snp.rbp_change"), sep="\t", header=0
-        )
-        snps = rbp_change.loc[:, ["#chrom", "pos", "ref", "alt"]]
-        rbp_change.drop(
-            ["#chrom", "pos", "ref", "alt", "ref_seq", "alt_seq"], axis=1, inplace=True
-        )
-
-        column_to_drop = [
-            "#chrom_snp",
-            "start_snp",
-            "end_snp",
-            "ref",
-            "alt",
-            "feature",
-            "gene_id",
-            "chrom",
-            "start",
-            "end",
-            "score",
-        ]
-        protein_feature = pd.read_csv(
-            os.path.join(out_dir_tmp, "snp.protein_feature"), sep="\t", header=0
-        )
-        protein_feature.drop(column_to_drop, axis=1, inplace=True)
-
-        junction_strength = pd.read_csv(
-            os.path.join(out_dir_tmp, "snp.junc"),
-            sep="\t",
-            header=0,
-            usecols=["aic", "dic", "aic_change", "dic_change"],
-        )
-
-        phylop = pd.read_csv(
-            os.path.join(out_dir_tmp, "snp.phylop"),
-            sep="\t",
-            header=0,
-            usecols=["phylop1", "phylop3", "phylop7"],
-        )
-
-        result = pd.concat(
-            [snps, rbp_change, protein_feature, junction_strength, phylop], axis=1
-        )
-        result.to_csv(
-            os.path.join(self.out_dir, "snp.features.txt"),
-            sep="\t",
-            index=False,
-            na_rep="NA",
-        )
-
-        pred = os.path.join(self.out_dir, "features.txt")
-        features = os.path.join(self.out_dir, "snp.features.txt")
-        # temp string to hold prediciton.txt data
-        pred_string = ""
-        # read in data of both files
-        with open(pred) as dbFile, open(features) as snpFile:
-            for line in dbFile:
-                pred_string += line
-            for line in snpFile:
-                pred_string += line
-        # write combined data to snp.features.txt
-        with open(features, "w") as out_f:
-            out_f.write(pred_string)
-        data = csv.reader(features, delimiter="\t")
-        # sort snp.features.txt
-        data = pd.read_csv(features)
-        data.sort_index(axis=0)
-        data.to_csv(features, sep="\t", header=False, index=False)
 
 
 def main():
--- regsnp_intron/predictor/predictor.py
+++ regsnp_intron/predictor/predictor.py
@@ -10,74 +10,96 @@ from sklearn.externals import joblib
 
 
 class Predictor(object):
-    def __init__(self, on_ss_imp, off_ss_imp, on_ss_clf, off_ss_clf, on_ss_fpr_tpr, off_ss_fpr_tpr):
+    def __init__(
+        self,
+        on_ss_imp,
+        off_ss_imp,
+        on_ss_clf,
+        off_ss_clf,
+        on_ss_fpr_tpr,
+        off_ss_fpr_tpr,
+    ):
         self.on_ss_imp = joblib.load(on_ss_imp)
         self.off_ss_imp = joblib.load(off_ss_imp)
         self.on_ss_clf = joblib.load(on_ss_clf)
         self.off_ss_clf = joblib.load(off_ss_clf)
-        self.on_ss_fpr_tpr = pd.read_csv(on_ss_fpr_tpr, sep='\t')
-        self.off_ss_fpr_tpr = pd.read_csv(off_ss_fpr_tpr, sep='\t')
+        self.on_ss_fpr_tpr = pd.read_csv(on_ss_fpr_tpr, sep="\t")
+        self.off_ss_fpr_tpr = pd.read_csv(off_ss_fpr_tpr, sep="\t")
         self.logger = logging.getLogger(__name__)
 
-    def predict(self, ifname, ofname, skip=False):
+    def predict(self, ifname, ofname):
         """
         Predict diseasing-causing probability of all intronic SNVs
         :param ifname: m x n input file. m is the number of SNVs, n is the number of features.
         :param ofname: output file prefix.
         """
-        if (not skip):
-            self.logger.info('Loading all the features.')
-            data = pd.read_csv(ifname, sep='\t', header=0)
-            data_on_ss = data[(data['distance'] >= -13) & (data['distance'] <= 7) & (data['distance'] != 0)]
-            data_off_ss = data[(data['distance'] < -13) | (data['distance'] > 7)]
-            if not data_on_ss.empty:
-                self.logger.info('Predicting on splicing site SNVs.')
-                preds, scores = self._predict_on_ss(data_on_ss)
-                fpr, tpr, disease = self._cal_fpr_tpr(scores, True)
-                data_on_ss.insert(4, 'splicing_site', ['on'] * len(preds))
-                data_on_ss.insert(4, 'fpr', fpr)
-                data_on_ss.insert(4, 'tpr', tpr)
-                data_on_ss.insert(4, 'prob', scores[:, 1])
-                data_on_ss.insert(4, 'disease', disease)
-            else:
-                data_on_ss.insert(4, 'splicing_site', '')
-                data_on_ss.insert(4, 'fpr', 0.0)
-                data_on_ss.insert(4, 'tpr', 0.0)
-                data_on_ss.insert(4, 'prob', 0.0)
-                data_on_ss.insert(4, 'disease', '')
-            if not data_off_ss.empty:
-                self.logger.info('Predicting off splicing site SNVs')
-                preds, scores = self._predict_off_ss(data_off_ss)
-                fpr, tpr, disease = self._cal_fpr_tpr(scores, False)
-                data_off_ss.insert(4, 'splicing_site', ['off'] * len(preds))
-                data_off_ss.insert(4, 'fpr', fpr)
-                data_off_ss.insert(4, 'tpr', tpr)
-                data_off_ss.insert(4, 'prob', scores[:, 1])
-                data_off_ss.insert(4, 'disease', disease)
-            else:
-                data_off_ss.insert(4, 'splicing_site', '')
-                data_off_ss.insert(4, 'fpr', 0.0)
-                data_off_ss.insert(4, 'tpr', 0.0)
-                data_off_ss.insert(4, 'prob', 0.0)
-                data_off_ss.insert(4, 'disease', '')
-            self.logger.info('Generate final output.')
-            result = pd.concat([data_on_ss, data_off_ss]).\
-                sort_values(by=['#chrom', 'pos'])
-            result.to_csv(ofname + '.txt', sep='\t', na_rep='NA', index=False)
-            records = result.loc[:, ['#chrom', 'pos', 'ref', 'alt', 'disease', 'prob', 'tpr', 'fpr', 'splicing_site', 'name', 'strand']].\
-                to_dict(orient='records')
-            json.dump({"data": records}, open(ofname + '.json', 'w'))
-        # TODO else for merging DB data with calculated data
-        # this might not be necessary, might only have to catch errors
-        # else:
-        #     data = pd.read_csv(ifname, sep='\t', header=0)
+        self.logger.info("Loading all the features.")
+        data = pd.read_csv(ifname, sep="\t", header=0)
+        data_on_ss = data[
+            (data["distance"] >= -13)
+            & (data["distance"] <= 7)
+            & (data["distance"] != 0)
+        ]
+        data_off_ss = data[(data["distance"] < -13) | (data["distance"] > 7)]
+        if not data_on_ss.empty:
+            self.logger.info("Predicting on splicing site SNVs.")
+            preds, scores = self._predict_on_ss(data_on_ss)
+            fpr, tpr, disease = self._cal_fpr_tpr(scores, True)
+            data_on_ss.insert(4, "splicing_site", ["on"] * len(preds))
+            data_on_ss.insert(4, "fpr", fpr)
+            data_on_ss.insert(4, "tpr", tpr)
+            data_on_ss.insert(4, "prob", scores[:, 1])
+            data_on_ss.insert(4, "disease", disease)
+        else:
+            data_on_ss.insert(4, "splicing_site", "")
+            data_on_ss.insert(4, "fpr", 0.0)
+            data_on_ss.insert(4, "tpr", 0.0)
+            data_on_ss.insert(4, "prob", 0.0)
+            data_on_ss.insert(4, "disease", "")
+        if not data_off_ss.empty:
+            self.logger.info("Predicting off splicing site SNVs")
+            preds, scores = self._predict_off_ss(data_off_ss)
+            fpr, tpr, disease = self._cal_fpr_tpr(scores, False)
+            data_off_ss.insert(4, "splicing_site", ["off"] * len(preds))
+            data_off_ss.insert(4, "fpr", fpr)
+            data_off_ss.insert(4, "tpr", tpr)
+            data_off_ss.insert(4, "prob", scores[:, 1])
+            data_off_ss.insert(4, "disease", disease)
+        else:
+            data_off_ss.insert(4, "splicing_site", "")
+            data_off_ss.insert(4, "fpr", 0.0)
+            data_off_ss.insert(4, "tpr", 0.0)
+            data_off_ss.insert(4, "prob", 0.0)
+            data_off_ss.insert(4, "disease", "")
+        self.logger.info("Generate final output.")
+        result = pd.concat([data_on_ss, data_off_ss]).sort_values(by=["#chrom", "pos"])
+        result.to_csv(ofname + ".txt", sep="\t", na_rep="NA", index=False)
+        records = result.loc[
+            :,
+            [
+                "#chrom",
+                "pos",
+                "ref",
+                "alt",
+                "disease",
+                "prob",
+                "tpr",
+                "fpr",
+                "splicing_site",
+                "name",
+                "strand",
+            ],
+        ].to_dict(orient="records")
+        json.dump({"data": records}, open(ofname + ".json", "w"))
 
     def _predict_on_ss(self, data):
         """
         Predict diseasing-causing probability of on-splicing-site SNVs
         :param data: m x n pandas data frame. m is the number of SNVs, n is the number of features.
         """
-        data = data.drop(['#chrom', 'pos', 'ref', 'alt', 'name', 'strand', 'distance'], axis=1)
+        data = data.drop(
+            ["#chrom", "pos", "ref", "alt", "name", "strand", "distance"], axis=1
+        )
         data = self.on_ss_imp.transform(data)
         preds = self.on_ss_clf.predict(data)
         scores = self.on_ss_clf.predict_proba(data)
@@ -88,8 +110,20 @@ class Predictor(object):
         Predict diseasing-causing probability of off-splicing-site SNVs
         :param data: m x n pandas data frame. m is the number of SNVs, n is the number of features.
         """
-        data = data.drop(['#chrom', 'pos', 'ref', 'alt', 'name', 'strand', 'distance', 'aic_change', 'dic_change'],
-                         axis=1)
+        data = data.drop(
+            [
+                "#chrom",
+                "pos",
+                "ref",
+                "alt",
+                "name",
+                "strand",
+                "distance",
+                "aic_change",
+                "dic_change",
+            ],
+            axis=1,
+        )
         data = self.off_ss_imp.transform(data)
         preds = self.off_ss_clf.predict(data)
         scores = self.off_ss_clf.predict_proba(data)
@@ -107,33 +141,37 @@ class Predictor(object):
             fpr_tpr = self.on_ss_fpr_tpr
         else:
             fpr_tpr = self.off_ss_fpr_tpr
-        fpr = [fpr_tpr.fpr[(abs(x - fpr_tpr.pvalue)).idxmin()] for x in scores[:,1]]
-        tpr = [fpr_tpr.tpr[(abs(x - fpr_tpr.pvalue)).idxmin()] for x in scores[:,1]]
+        fpr = [fpr_tpr.fpr[(abs(x - fpr_tpr.pvalue)).idxmin()] for x in scores[:, 1]]
+        tpr = [fpr_tpr.tpr[(abs(x - fpr_tpr.pvalue)).idxmin()] for x in scores[:, 1]]
         cutoff = [0.0, 0.05, 0.1, 1.1]
-        disease_category = ['D', 'PD', 'B']
+        disease_category = ["D", "PD", "B"]
         disease = np.digitize(fpr, cutoff)
         disease = [disease_category[i - 1] for i in disease]
         return fpr, tpr, disease
 
 
 def main():
-    parser = argparse.ArgumentParser(description='''
+    parser = argparse.ArgumentParser(
+        description="""
             Given model files and SNV features generated by feature calculator,
-            predict the disease-causing probability.''')
-    parser.add_argument('db_path',
-            help='path to models')
-    parser.add_argument('sfname',
-            help='SNV file with features generated by feature calculator')
-    parser.add_argument('ofname',
-            help='output file prefix')
+            predict the disease-causing probability."""
+    )
+    parser.add_argument("db_path", help="path to models")
+    parser.add_argument(
+        "sfname", help="SNV file with features generated by feature calculator"
+    )
+    parser.add_argument("ofname", help="output file prefix")
     args = parser.parse_args()
-    predictor = Predictor(os.path.join(args.db_path, 'on_ss_imp.pkl'),
-                          os.path.join(args.db_path, 'off_ss_imp.pkl'),
-                          os.path.join(args.db_path, 'on_ss_clf.pkl'),
-                          os.path.join(args.db_path, 'off_ss_clf.pkl'),
-                          os.path.join(args.db_path, 'on_ss_fpr_tpr.txt'),
-                          os.path.join(args.db_path, 'off_ss_fpr_tpr.txt'))
+    predictor = Predictor(
+        os.path.join(args.db_path, "on_ss_imp.pkl"),
+        os.path.join(args.db_path, "off_ss_imp.pkl"),
+        os.path.join(args.db_path, "on_ss_clf.pkl"),
+        os.path.join(args.db_path, "off_ss_clf.pkl"),
+        os.path.join(args.db_path, "on_ss_fpr_tpr.txt"),
+        os.path.join(args.db_path, "off_ss_fpr_tpr.txt"),
+    )
     predictor.predict(args.sfname, args.ofname)
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
